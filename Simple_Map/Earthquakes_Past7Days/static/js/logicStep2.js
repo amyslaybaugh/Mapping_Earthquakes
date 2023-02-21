fd28ef206fcf2013195fac980527eeb24d824796
amyslaybaugh <116078337+amyslaybaugh@users.noreply.github.com>
2023-02-21 13:07:46 -0600

    Updated Earthquake map

--- Simple_Map/Earthquakes_Past7Days/static/js/logicStep2.js
+++ Simple_Map/Earthquakes_Past7Days/static/js/logicStep2.js
@@ -1,7 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -32,28 +31,39 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// // Accessing the Toronto neighborhoods GeoJSON URL.
-// let torontoHoods = "https://raw.githubusercontent.com/amyslaybaugh/Mapping_Earthquakes/main/torontoNeighborhoods.json";
-
 // Retrieve the earthquake GeoJSON data.
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+let earthquakes = ("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson");
+  console.log(earthquakes)
+
+let myStyle = function styleInfo(feature) {
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: "#ffae42",
+    color: "#000000",
+    radius: getRadius(),
+    stroke: true,
+    weight: 0.5
+  };
+   function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+      }
+      return magnitude * 4;
+  };
+};  
+
+d3.json(earthquakes).then(function(data) {
+  console.log(data);
+  L.geoJSON(data, {
+    
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+                console.log(data);
+                return L.circleMarker(latlng);
+            },
+          // We set the style for each circleMarker using our styleInfo function.
+    style: myStyle
+  }).addTo(map);
+    
 });
-
-// // Grabbing our GeoJSON data.
-// d3.json(torontoHoods).then(function(data) {
-//     console.log(data);
-//   // Creating a GeoJSON layer with the retrieved data.
-//   L.geoJSON(data, {
-//     color: "#ffffa1",
-//     weight: 1,
-//     fillColor: "blue",
-//     //We turn each feature into a marker on the map.
-//     onEachFeature: function(features, layer) {
-//       console.log(layer);
-//       console.log(features);
-//       layer.bindPopup("Neighborhood:"+ features.properties.AREA_NAME);
-//     }
-// }).addTo(map);
-
